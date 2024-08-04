72976c5fe8a8e24afc5cec7afd1b79ef1946f6b2
unknown <carlos.sala.munoz@gmail.com>
2024-08-04 14:50:25 -0400

    se migra de livedata a flow

--- app/src/main/java/com/example/examplemvvm/ui/QuoteViewModel.kt
+++ app/src/main/java/com/example/examplemvvm/ui/QuoteViewModel.kt
@@ -1,12 +1,13 @@
 package com.example.examplemvvm.ui
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.examplemvvm.domain.GetQuoteUseCase
 import com.example.examplemvvm.domain.GetRandomQuoteUseCase
 import com.example.examplemvvm.domain.model.Quote
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -16,11 +17,11 @@ class QuoteViewModel @Inject constructor(
     private val getRandomQuoteUseCase: GetRandomQuoteUseCase
 ) : ViewModel() {
 
-    private val _quoteModel = MutableLiveData<Quote?>()
-    val quoteModel: MutableLiveData<Quote?> = _quoteModel
+    private val _quoteModel = MutableStateFlow<Quote?>(null)
+    val quoteModel: StateFlow<Quote?> = _quoteModel
 
-    private val _isLoading = MutableLiveData<Boolean>()
-    val isLoading: MutableLiveData<Boolean> = _isLoading
+    private val _isLoading = MutableStateFlow(false)
+    val isLoading: StateFlow<Boolean> = _isLoading
 
     // it is no  necessary with dependency injection
     /*  var getQuoteUseCase = GetQuoteUseCase()
@@ -30,11 +31,11 @@ class QuoteViewModel @Inject constructor(
     fun onCreate() {
 
         viewModelScope.launch {
-            _isLoading.postValue(true)
+            _isLoading.value = true
             val result = getQuoteUseCase()
             if (result.isNotEmpty()) {
-                _quoteModel.postValue(result[0])
-                _isLoading.postValue(false)
+                _quoteModel.value = result[0]
+                _isLoading.value = false
             }
         }
     }
@@ -43,15 +44,15 @@ class QuoteViewModel @Inject constructor(
     fun randomQuote() {
 
         viewModelScope.launch {
-            _isLoading.postValue(true)
+            _isLoading.value = true
             val quote = getRandomQuoteUseCase()
             if (quote != null) {
-                _quoteModel.postValue(quote)
+                _quoteModel.value = quote
             }
             /*
                val currentQuote: QuoteModel = QuoteProvider.random()
                quoteModel.postValue(currentQuote)*/
-            _isLoading.postValue(false)
+            _isLoading.value = false
         }
     }
 }
